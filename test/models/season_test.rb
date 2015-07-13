78bb0a4ce86c1e5a9f1d94464f294936aa8df4f0
Marcelino Llano <hello@marcelinollano.com>
2015-07-13 23:59:16 +0200

    Add some more test cases for seasons

--- test/models/season_test.rb
+++ test/models/season_test.rb
@@ -2,6 +2,18 @@ require "test_helper"
 
 describe Season do
   
-  it "must be tested"
+  it "must be created" do
+    Season.count.must_be :==, 3
+  end
+  
+  it "must have a country"  
+  
+  it "must have a state"
+  
+  it "must have a food"
+  
+  it "must have a month"
+  
+  it "must have a valid month"
   
 end
