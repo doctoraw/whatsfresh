afbb80c7b581c0b87b7c77296a46f7adf1ebf70d
Marcelino Llano <hello@marcelinollano.com>
2015-07-14 00:32:27 +0200

    Load an arbitrary number of countries from a json file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,71 +1,12 @@
 # This file seeds the database with the initial data required.
-# OPTIMIZE: Make this for any number of countries from a JSON file.
 
-countries = [
-     ["United States", "US"]
-]
+require 'json'
+countries = File.read(File.dirname(__FILE__) + '/countries.json')
+countries = JSON.parse(countries)
 
-countries.each do |country|
-  c = Country.new(:name => country[0], :code => country[1])
-  c.save!
-end
-
-us = [
-     ["Alabama","AL"],
-     ["Alaska","AK"],
-     ["Arizona","AZ"],
-     ["Arkansas","AR"],
-     ["California","CA"],
-     ["Colorado","CO"],
-     ["Connecticut","CT"],
-     ["Delaware","DE"],
-     ["District of Columbia","DC"],
-     ["Florida","FL"],
-     ["Georgia","GA"],
-     ["Hawaii","HI"],
-     ["Idaho","ID"],
-     ["Illinois","IL"],
-     ["Indiana","IN"],
-     ["Iowa","IA"],
-     ["Kansas","KS"],
-     ["Kentucky","KY"],
-     ["Louisiana","LA"],
-     ["Maine","ME"],
-     ["Montana","MT"],
-     ["Nebraska","NE"],
-     ["Nevada","NV"],
-     ["New Hampshire","NH"],
-     ["New Jersey","NJ"],
-     ["New Mexico","NM"],
-     ["New York","NY"],
-     ["North Carolina","NC"],
-     ["North Dakota","ND"],
-     ["Ohio","OH"],
-     ["Oklahoma","OK"],
-     ["Oregon","OR"],
-     ["Maryland","MD"],
-     ["Massachusetts","MA"],
-     ["Michigan","MI"],
-     ["Minnesota","MN"],
-     ["Mississippi","MS"],
-     ["Missouri","MO"],
-     ["Pennsylvania","PA"],
-     ["Rhode Island","RI"],
-     ["South Carolina","SC"],
-     ["South Dakota","SD"],
-     ["Tennessee","TN"],
-     ["Texas","TX"],
-     ["Utah","UT"],
-     ["Vermont","VT"],
-     ["Virginia","VA"],
-     ["Washington","WA"],
-     ["West Virginia","WV"],
-     ["Wisconsin","WI"],
-     ["Wyoming","WY"]
-]
-
-us.each do |state|
-  country_id = Country.where('name = ?', 'United States').first.id
-  state = State.new(:name => state[0], :code => state[1], :country_id => country_id)
-  state.save!
+countries.each do |c|
+  country = Country.create(:name => c[0], :code => c[1]['code'])
+  c[1]['states'].each do |s|
+    State.create(:name => s[0], :code => s[1], :country_id => country.id)
+  end
 end
